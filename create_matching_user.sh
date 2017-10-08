303c125a3b68c09da97aa4257fa703713701c4fb
Anton Kozlov <drakon.mega@gmail.com>
2017-10-08 22:52:54 +0300

    Create "matching user" group if needed
    Fixes use on travis

--- create_matching_user.sh
+++ create_matching_user.sh
@@ -6,6 +6,10 @@ uid=$(stat -c %u $target_dir)
 gid=$(stat -c %g $target_dir)
 
 mkdir -p $home
-chown $uid $home
+chown $uid:$gid $home
+
+if ! getent group $gid; then
+	groupadd -g $gid embox_user
+fi
 
 useradd -u $uid -g $gid -G sudo -d $home -s /bin/bash $user
